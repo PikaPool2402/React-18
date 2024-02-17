c6764016c849639c7371768587bb0065652d3c3a
PikaPool2402 <tigerkushgupta@gmail.com>
2024-02-17 22:20:40 +0530

    Finished JSX

--- 01. Fundamentals/src/index.js
+++ 01. Fundamentals/src/index.js
@@ -14,30 +14,20 @@ function BookList() {
     );
 }
 
+const author = "Jordan Moore";
 const Book = () => {
+    const title = "Interesting Facts For Curious Minds";
     return (
         <article className='book'>
-            <Image />
-            <Title />
-            <Author />
+            <img
+                src='https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg'
+                alt='Interesting Facts For Curious Minds'
+            />
+            <h2>{title}</h2>
+            <h4>{author}</h4>
         </article>
     );
 };
-
-const Image = () => (
-    <img
-        src='https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg'
-        alt='Interesting Facts For Curious Minds'
-    />
-);
-const Title = () => {
-    return <h2>Interesting Facts For Curious Minds</h2>;
-};
-const Author = () => (
-    <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}>
-        Jordan Moore
-    </h4>
-);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
