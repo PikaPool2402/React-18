e7771a276ab03d9fc3f1e9b0d7ab2ff68f444cb7
PikaPool2402 <tigerkushgupta@gmail.com>
2024-02-17 23:53:36 +0530

    Somewhat Dynamic Setup

--- 01. Fundamentals/src/index.js
+++ 01. Fundamentals/src/index.js
@@ -3,16 +3,30 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const author = "Jordan Moore";
-const title = "Interesting Facts For Curious Minds";
-const img =
-    "https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg";
+const firstBook = {
+    author: "Jordan Moore",
+    title: "Interesting Facts For Curious Minds",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg",
+};
+const secondBook = {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+};
 
 function BookList() {
     return (
         <section className='booklist'>
-            <Book author={author} title={title} img={img} />
-            <Book author={author} title={title} img={img} />
+            <Book
+                author={firstBook.author}
+                title={firstBook.title}
+                img={firstBook.img}
+            />
+            <Book
+                author={secondBook.author}
+                title={secondBook.title}
+                img={secondBook.img}
+            />
         </section>
     );
 }
