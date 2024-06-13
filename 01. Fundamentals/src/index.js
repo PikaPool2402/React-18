71a670dd7bcefed1929d59d3f618197dca576c80
PikaPool2402 <tigerkushgupta@gmail.com>
2024-06-13 13:12:51 +0530

    Finished Data Flow

--- 01. Fundamentals/src/index.js
+++ 01. Fundamentals/src/index.js
@@ -19,28 +19,33 @@ const books = [
 ];
 
 function BookList() {
+    const someValue = "shakeAndBake";
+    // how to pass down the function as a prop?
+    const displayValue = () => {
+        console.log(someValue);
+    };
+
     return (
         <section className='booklist'>
             {books.map((book) => {
-                return <Book {...book} key={book.id} />;
+                return (
+                    <Book {...book} key={book.id} displayValue={displayValue} />
+                );
             })}
         </section>
     );
 }
 
 const Book = (props) => {
-    const { img, title, author } = props;
-
-    const displayTitle = () => {
-        console.log(title);
-    };
+    const { img, title, author, displayValue } = props;
+    // grab the function from the prop and pass it to the onClick event!
 
     return (
         <article className='book'>
             <img src={img} alt={title} />
             <h2>{title}</h2>
             <h4>{author}</h4>
-            <button onClick={displayTitle}>Display Title!</button>
+            <button onClick={displayValue}>Click Me!</button>
         </article>
     );
 };
